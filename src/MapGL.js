888d5bbed93e3e2b2c17f0f5ae71facd516ea680
blinky3713 <blinky@foam.space>
2018-03-14 10:39:02 -0400

    use purescript arrays for LngLat

--- src/MapGL.js
+++ src/MapGL.js
@@ -1,17 +1,3 @@
 const MapGL = require('react-map-gl');
 
 exports.mapGL = MapGL.default;
-
-exports.lat = function (lngLat) {
-    return lngLat[1];
-};
-
-exports.lng = function (lngLat) {
-    return lngLat[0];
-};
-
-exports.makeLngLat = function (lng) {
-    return function (lat) {
-        return [lng, lat];
-    };
-};
