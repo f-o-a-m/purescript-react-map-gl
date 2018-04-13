a8e8a9ae1ccdf1d5f6fb7f69ffb6773048064146
Irakli Safareli <i.safareli@gmail.com>
2018-04-13 20:48:49 +0200

    wrap map in halogen component

--- example/map-halogen/src/Main.purs
+++ example/map-halogen/src/Main.purs
@@ -1,70 +1,12 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Uncurried (mkEffFn1)
-import Data.Maybe (fromJust)
-import DOM (DOM)
-import DOM.HTML (window)
-import DOM.HTML.Types (htmlDocumentToDocument)
-import DOM.HTML.Window as Window
-import DOM.Node.NonElementParentNode (getElementById)
-import DOM.Node.Types (Element, ElementId(..), documentToNonElementParentNode)
-import MapGL as MapGL
-import Partial.Unsafe (unsafePartial)
-import React as R
-import ReactDOM (render)
+import MapComponent (mapComponent)
+import Halogen.VDom.Driver (runUI)
+import Halogen.Aff as HA
 
-
-main :: forall eff. Eff (dom :: DOM | eff) Unit
-main = void  $ elm' >>= render (R.createFactory mapClass unit)
-  where
-    elm' :: Eff (dom :: DOM | eff) Element
-    elm' = do
-      win <- window
-      doc <- Window.document win
-      elm <- getElementById (ElementId "app") (documentToNonElementParentNode (htmlDocumentToDocument doc))
-      pure $ unsafePartial (fromJust elm)
-
-mapClass :: forall props . R.ReactClass props
-mapClass = R.createClass mapSpec
-
-mapSpec :: forall props eff . R.ReactSpec props MapGL.Viewport R.ReactElement (console :: CONSOLE, dom :: DOM | eff)
-mapSpec = R.spec' (const initialViewport) render
-  where
-    render this = R.readState this <#> \vp ->
-      R.createFactory MapGL.mapGL $
-        MapGL.mkProps vp
-          { onChangeViewport: mkEffFn1 $ \newVp -> do
-              log $ "Changed Viewport: " <> show newVp
-              void $ R.writeState this newVp
-          , onClick: mkEffFn1 $ \info -> do
-              log $ "Clicked map: (" <> show (MapGL.lng info.lngLat) <> ", " <> show (MapGL.lat info.lngLat) <> ")"
-          , mapStyle: mapStyle
-          , mapboxApiAccessToken: mapboxApiAccessToken
-          }
-
-initialViewport :: forall eff . Eff (dom :: DOM | eff) MapGL.Viewport
-initialViewport = do
-  win <- window
-  w <- Window.innerWidth win
-  h <- Window.innerHeight win
-  pure $
-    MapGL.Viewport { width: w
-                   , height: h
-                   , longitude: -74.00539284665783
-                   , latitude: 40.70544878575082
-                   , zoom: 10.822714855509464
-                   , pitch: 0.0
-                   , bearing: 0.0
-                   }
-
-
-mapStyle :: String
-mapStyle = "mapbox://styles/mapbox/dark-v9"
-
-mapboxApiAccessToken :: String
-mapboxApiAccessToken = "pk.eyJ1IjoiYmxpbmt5MzcxMyIsImEiOiJjamVvcXZtbGYwMXgzMzNwN2JlNGhuMHduIn0.ue2IR6wHG8b9eUoSfPhTuQ"
-
-
+main :: Eff (HA.HalogenEffects ()) Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI mapComponent unit body
