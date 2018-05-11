1a993641d01de2317e64fc843c6d46542891918b
Irakli Safareli <i.safareli@gmail.com>
2018-05-11 11:26:30 +0200

    add HTMLOverlay

---
+++ src/MapGL/HTMLOverlay.purs
@@ -0,0 +1,19 @@
+module MapGL.HTMLOverlay where
+
+import Control.Monad.Eff (Eff)
+import React as R
+import WebMercator.Viewport (Pixel, LngLat)
+
+type RedrawArgs =
+  { width :: Number
+  , height :: Number
+  , isDragging :: Boolean
+  , project :: LngLat -> Pixel
+  , unproject :: Pixel -> LngLat
+  }
+
+type Props eff =
+  { redraw :: RedrawArgs -> Eff eff R.ReactElement
+  }
+
+foreign import overlay :: forall eff. R.ReactClass (Props eff)
