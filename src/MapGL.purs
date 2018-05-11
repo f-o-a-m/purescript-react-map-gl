75e0e92780fe7b5a3027e4fd6bd8b0047f9e0061
Irakli Safareli <i.safareli@gmail.com>
2018-05-11 11:26:07 +0200

    import ViewportR and LngLat from WebMercator.Viewport

--- src/MapGL.purs
+++ src/MapGL.purs
@@ -1,7 +1,5 @@
 module MapGL
   ( Viewport(..)
-  , ViewportR
-  , LngLat
   , OnViewportChange
   , ClickInfo
   , OnClickMap
@@ -9,64 +7,30 @@ module MapGL
   , MapPropsR
   , mkProps
   , mapGL
-  , lng
-  , lat
-  , makeLngLat
   ) where
 
 import Prelude
 
 import Control.Monad.Eff (kind Effect)
 import Control.Monad.Eff.Uncurried (EffFn1)
-import Data.Array ((!!))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.Maybe (fromJust)
 import Data.Newtype (class Newtype)
 import Data.Record.Builder (build, merge)
-import Partial.Unsafe (unsafePartial)
 import React as R
+import WebMercator.Viewport (LngLat, ViewportR)
+
+-- TODO add animation props to Viewport
 
 -- | `Viewport` is the most basic state type for the map component.
 newtype Viewport =
   Viewport (Record (ViewportR ()))
-
-type ViewportR r =
-  ( width :: Int
-  , height :: Int
-  , latitude :: Number
-  , longitude :: Number
-  , zoom :: Number
-  , bearing :: Number
-  , pitch :: Number
-  | r
-  )
 
 derive instance genericViewport :: Generic Viewport _
 derive instance newtypeViewport :: Newtype Viewport _
 derive instance eqViewport :: Eq Viewport
 
 instance showViewport :: Show Viewport where
-  show = genericShow
-
--- | LngLat is used because the native representation of a set of
--- | coordinates is an array of length 2.
-newtype LngLat = LngLat (Array Number)
-
-lng :: LngLat -> Number
-lng (LngLat lnglat) = unsafePartial fromJust $ lnglat !! 0
-
-lat :: LngLat -> Number
-lat (LngLat lnglat) = unsafePartial fromJust $ lnglat !! 1
-
-makeLngLat :: Number -> Number -> LngLat
-makeLngLat x y = LngLat [x,y]
-
-derive instance genericLngLat :: Generic LngLat _
-derive instance newtypeLngLat :: Newtype LngLat _
-derive instance eqLngLat :: Eq LngLat
-
-instance showLngLat :: Show LngLat where
   show = genericShow
 
 -- | A handler to be run whenever the viewport changes
