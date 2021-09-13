b5d5205bdea77427771d9f8f3854efd1e97fade0
kejace <kejace@gmail.com>
2021-09-13 17:21:19 -0400

    Fixes

--- example/signal-halogen/src/Container.purs
+++ example/signal-halogen/src/Container.purs
@@ -4,6 +4,7 @@ module Container
   ) where
 
 import Prelude
+
 import Control.Lazy (fix)
 import Data.Foldable (for_)
 import Data.Int (toNumber)
@@ -27,11 +28,9 @@ import Web.HTML.HTMLElement as HTMLElement
 import Web.HTML.Window as Window
 import Control.Monad.Rec.Class (forever)
 
-type Slot f
-  = H.Slot f Map.MapMessages
+type Slot f = H.Slot f Map.MapMessages
 
-type State
-  = Maybe (Bus.BusW Map.Commands)
+type State = Maybe (Bus.BusW Map.Commands)
 
 data Action
   = Initialize
@@ -46,6 +45,7 @@ mapComponent =
     , eval
     }
   where
+
   render :: forall s. State -> H.ComponentHTML Action s m
   render = const $
     HH.div 
@@ -55,14 +55,14 @@ mapComponent =
           [ HP.class_ $ HH.ClassName "btn-toggle"
           , HE.onClick $ \_ -> ToggleHeatmap
           ]
+          [ HH.text "Toggle heatmap" ]
+      ]
 
 eval :: forall f i s m. MonadAff m => H.HalogenQ f Action i ~> H.HalogenM State Action s Map.MapMessages m
-eval =
-  H.mkEval
-    $ H.defaultEval
-        { handleAction = handleAction
-        , initialize = Just Initialize
-        }
+eval = H.mkEval $ H.defaultEval 
+  { handleAction = handleAction
+  , initialize = Just Initialize
+  }
   where
     handleAction :: Action -> H.HalogenM State Action s Map.MapMessages m Unit
     handleAction = case _ of
